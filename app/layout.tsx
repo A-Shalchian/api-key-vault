3f2e109e7abcf78a1e2dc13e7bc0179cec7dee7a
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-07-22 02:32:00 -0400

    style: wrap layout in ThemeProvider for improved theming support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/app/components/Navbar";
+import { ThemeProvider } from "@/app/contexts/ThemeContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <main>
-          {children}
-        </main>
+        <ThemeProvider>
+          <Navbar />
+          <main>
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
